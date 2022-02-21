7469effc9c91ad3b5c908778b81885e92919172b
aidenrosen <aidenrrosen@gmail.com>
2022-02-21 17:41:47 -0500

    Added fish

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -5,15 +5,14 @@ public class AnimalPicker
 	public static void main(String[] args)
 	{
 		String animal = askUser();
-
 	}
 
 	public static String askUser()
 	{
 		Scanner in = new Scanner(System.in);
-		System.out.println("Would you like to see a dog or a cat?");
+		System.out.println("Would you like to see a dog, cat, or fish?");
 
-		switch(in.nextLine())
+		switch(in.nextLine().toLowerCase())
 		{
 			case "dog":
 				System.out.println(" / \\__\n  (    @\\___\n  /         O\n /   (_____/\n/_____/   U");
@@ -21,6 +20,9 @@ public class AnimalPicker
 			case "cat":
 				System.out.println(" _._     _,-\'\"\"`-._\n(,-.`._,\'(       |\\`-/|\n    `-.-\' \\ )-`( , o o)\n          `-    \\`_`\"\'-");
 				return "cat";
+			case "fish":
+				System.out.println("|\\   \\\\\\\\__     o\n| \\_/    o \\    o \n> _   (( <_  oo  \n| / \\__+___/      \n|/     |/");
+				return "fish";
 			default:
 				System.out.println("Invalid response");
 				break;
