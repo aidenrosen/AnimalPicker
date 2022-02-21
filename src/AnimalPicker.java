a01f902516561fe82010b7b78bf0c981ecdbfc45
aidenrosen <aidenrrosen@gmail.com>
2022-02-21 17:27:46 -0500

    Added error on invalid response

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -5,21 +5,26 @@ public class AnimalPicker
 	public static void main(String[] args)
 	{
 		String animal = askUser();
-		switch(animal)
-		{
-			case "dog":
-				System.out.println("DOG ASCII ART HERE");
-				break;
-			case "cat":
-				System.out.println("CAT ASCII ART HERE");
-				break;
-		}
+
 	}
 
 	public static String askUser()
 	{
 		Scanner in = new Scanner(System.in);
 		System.out.println("Would you like to see a dog or a cat?");
-		return in.nextLine().toLowerCase();
+
+		switch(in.nextLine())
+		{
+			case "dog":
+				System.out.println("DOG ASCII ART HERE");
+				return "dog";
+			case "cat":
+				System.out.println("CAT ASCII ART HERE");
+				return "cat";
+			default:
+				System.out.println("Invalid response");
+				break;
+		}
+		return null;
 	}
 }
