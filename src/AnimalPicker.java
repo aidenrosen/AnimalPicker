8d5d867dd90f3a553582bbaa7a2100153a41932e
aidenrosen <aidenrrosen@gmail.com>
2022-02-21 16:41:43 -0500

    Ask for user input

--- src/AnimalPicker.java
+++ src/AnimalPicker.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class AnimalPicker
 {
 	public static void main(String[] args)
 	{
+		String animal = askUser();
+		switch(animal)
+		{
+			case "dog":
+				System.out.println("DOG ASCII ART HERE");
+				break;
+			case "cat":
+				System.out.println("CAT ASCII ART HERE");
+				break;
+		}
+	}
 
+	public static String askUser()
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Would you like to see a dog or a cat?");
+		return in.nextLine().toLowerCase();
 	}
 }
